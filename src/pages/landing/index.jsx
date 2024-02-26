018445eb64f96856ceca1dacb8bb81cb87e258c7
nasith <nasith@pepul.com>
2024-02-26 16:18:44 +0530

    tag chnages

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -82,9 +82,9 @@ export default function Landing() {
         <div className='flex flex-col justify-start items-center w-full h-full bg-[#010306] relative overflow-hidden px-[20px] sm:px-0'>
             <img src={Logo} alt='logo' className='z-[1] mt-[50px] w-[75px] sm:w-auto' />
             <video src={Main} className='absolute z-[0] left-[50%] -translate-x-[50%] sm:w-[460px] -top-[20px] sm:-top-[30px]' autoPlay muted loop />
-            <p className='DarkerGrostesqueSemibold text-[30px] sm:text-[50px] xl:text-[80px] text-[#FEF9F1] z-[1] mt-[80px] leading-3'>One intelligent App</p>
-            <p className='DarkerGrostesqueSemibold text-[30px] sm:text-[60px] xl:text-[110px] bg-gradient-to-l from-[#FDD220] to-[#FFFFFF] bg-clip-text text-transparent z-[1] text-center'>to manage your work and life </p>
-            <p className='DarkerGrostesqueMedium text-[14px] sm:text-[24px] text-[#FEF9F1] mt-[20px] z-[1]'>Built with intelligence & empathy</p>
+            <h1 className='DarkerGrostesqueSemibold text-[30px] sm:text-[50px] xl:text-[80px] text-[#FEF9F1] z-[1] mt-[80px] leading-3'>One intelligent App</h1>
+            <h1 className='DarkerGrostesqueSemibold text-[30px] sm:text-[60px] xl:text-[110px] bg-gradient-to-l from-[#FDD220] to-[#FFFFFF] bg-clip-text text-transparent z-[1] text-center'>to manage your work and life </h1>
+            <h1 className='DarkerGrostesqueMedium text-[14px] sm:text-[24px] text-[#FEF9F1] mt-[20px] z-[1]'>Built with intelligence & empathy</h1>
             <div className='max-w-[500px]'>
                 <div className='bg-transparent w-full p-[1px] rounded-[10px] mt-[40px] z-[1] animation relative'>
                     <div className='w-full bg-[#000] rounded-[10px] z-[5] flex gap-[10px] relative p-[10px]'>
@@ -98,10 +98,10 @@ export default function Landing() {
             <div className='w-full overflow-hidden z-[3] radialGradient'><p className='font-semibold text-[14px] sm:text-[18px] lg:text-[26px] xl:text-[35px] text-[#FEF9F1] text-center mt-[50px] w-10/12 sm:w-9/12 lg:w-8/12 xl:w-1/2 mx-auto mb-[20px]'>it’s not for everyone</p></div>
             <div className=' p-[10px] border-[#FDD320]/[20%] h-[70px] sm:h-[90px] lg:h-[100px] xl:h-[150px] border-[1px] rounded-[20px] DarkerGrostesqueSemibold text-[24px] sm:text-[36px] lg:text-[50px] xl:text-[80px] max-w-[300px] sm:max-w-[400px] lg:max-w-[730px] w-full mx-auto relative mb-[80px] sm:mb-[120px] xl:mb-[100px]mb-[150px]'>
                 <div className='flex gap-[20px] absolute left-[35%] -translate-x-[50%]'>
-                    <p className='text-[#FEF9F1]'>It’s for</p>
+                    <h3 className='text-[#FEF9F1]'>It’s for</h3>
                     <div className='inline-block'>
                         {textArray.map((text, index) => (
-                            <p key={index} className={`verticalFilp absolute z-0 text-[#FFE269] ${currentIndex === index ? 'current-index' : 'hidden'}`}>{text}</p>
+                            <h3 key={index} className={`verticalFilp absolute z-0 text-[#FFE269] ${currentIndex === index ? 'current-index' : 'hidden'}`}>{text}</h3>
                         ))}
                     </div>
                     <div className='h-[22px] sm:h-[28px] lg:h-[20px] xl:h-[30px] w-[100px] rounded-b-[20px] sm:w-[250px] lg:w-[300px] xl:w-[480px] mx-[10px] bg-[#010306] z-[2] absolute -bottom-[22px] sm:-bottom-[23px] lg:-bottom-[12px] xl:-bottom-[17px] left-[52px] sm:left-[0px]'></div>
@@ -117,7 +117,7 @@ export default function Landing() {
                 <div>
                     <div className='flex gap-[15px] lg:gap-[20px] items-center sm:justify-end mt-[20px] sm:mt-0'>
                         <p className='text-[#858585] text-[14px] lg:text-[16px] font-medium'>Contact: </p>
-                        <div className='text-center flex items-center justify-center gap-[5px]'>
+                        <div className='text-center flex items-center justify-center gap-[5px] cursor-pointer'>
                             <IoMdMail className="stroke-[#FFFFFF] fill-[#FFF] w-[16px] h-[16px] lg:w-[20px] lg:h-[20px] flex shrink-0" />
                             <p className='font-light text-[#FEF9F1] text-[13px] lg:text-[16px]' onClick={handleEmailClick}>hello@get1.ai</p>
                         </div>
