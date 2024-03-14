f4dac261dc4cb37f9bbf427ac78ab2286a28836f
nasith <nasith@pepul.com>
2024-03-14 19:00:15 +0530

    new Home page

---
+++ src/pages/home 2/component/header/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Preview from "../../../../images/preview.svg";
+import previewone from "../../../../images/channel.svg"
+import Glow_2 from "../../../../images/Glowing2.svg";
+// import gif from "../../../../images/giffycanvas.gif"
+// import Elliped7 from "../../../../images/Ellipse7.svg"
+
+export default function Header() {
+  return (
+    <>
+      <section className="p-[20px] md:p-[30px] lg:p-[50px]">
+        <div className="mt-[20px]">
+          <div className="">
+            <h1 className="text-[#E4E8EB] text-[50px] text-center font-medium DarkerGrostesqueSemibold md:text-[50px] lg:text-[50px] xl:text-[75px]">Channel</h1>
+            <p className="text-[#E4E8EB] text-[14px] text-center md:text-[15px] lg:text-[18px] xl:text-[32px]">A Space to Communicate and get alligned with your team on Project and tasks   <span className="text-[#FFE269]"></span>  <br></br> </p>
+          </div>
+          <div className="grid grid-cols-1 mx-auto md:grid-cols-3 md:w-10/12 md:mx-auto lg:mt-[30px] lg:w-12/12  2xl:w-10/12 2xl:mx-auto">
+            <div className="mt-[20px] col-span-1">
+              <img src={previewone} className="w-fit h-[300px] mx-auto md:w-fit md:h-[250px] lg:w-fit lg:h-[320px] xl:w-full xl:h-[450px]" alt="noimg" />
+            </div>
+
+            {/* <div className="mt-[30px] ">
+                  <h1 className="text-[#E4E8EB] text-center text-[18px] sm:text-left  DarkerGrostesque font-medium	 leading-[22px] md:text-[18px] lg:text-[24px]  xl:text-[38px] xl:leading-[38.11px] 2xl:text-[50px] 2xl:leading-[50px]">Come & join together for<br></br><span className="text-[#FFE269]"> create great things</span></h1>
+                  <p className="text-[#E4E8EB] text-center mt-[10px] text-[13px]  sm:text-left md:text-[13px] md:w-10/12 md:!font-[500px] lg:w-8/12 lg:text-[16px] xl:text-[21px] xl:leading-[35px] xl:w-8/12 2xl:w-6/12 2xl:text-[26px] 2xl:mt-[20px]">Create a space for your community to come together. Build it yourselves, making a place that reflects the unity and identity of your people.</p>
+                  <div className="mx-auto flex flex-col justify-center items-center sm:justify-start sm:items-start px-[11px] md:px-[0px]">
+                    <div className="flex flex-row mt-[10px] xl:mt-[20px] w-8/12">
+                      <img src={dots}  className="" alt="noimg"/>
+                      <h1 className="text-[#E4E8EB] px-[10px] text-[15px] xl:text-[20px]">Easy Collabration</h1>
+                    </div>
+                    <div className="flex flex-row mt-[5px] md:mt-[8px] w-8/12">
+                      <img src={dots}  className="" alt="noimg"/>
+                      <h1 className="text-[#E4E8EB] px-[10px] text-[15px] xl:text-[20px]">Multiple Channels</h1>
+                    </div>
+                    <div className="flex flex-row mt-[5px] md:mt-[8px] w-8/12">
+                      <img src={dots}  className="" alt="noimg"/>
+                      <h1 className="text-[#E4E8EB] px-[10px]  text-[15px] xl:text-[20px]">Icons based profiles</h1>
+                    </div>
+                  </div>
+               </div> */}
+            <div className="mt-[20px] col-span-2">
+              <img src={Preview} className="" alt="noimg" />
+            </div>
+          </div>
+        </div>
+        <img src={Glow_2} alt='glow' className='md:absolute md:left-[3px] md:top-[300px] md:h-[500px] lg:h-[700px] lg:absolute lg:left-[3px] lg:top-[300px] xl:absolute xl:left-0 xl:top-[450px] 2xl:w-[500px] 2xl:h-[1000px] z-[0] hidden sm:block' />
+      </section>
+    </>
+
+
+
+
+
+  )
+}
---
+++ src/pages/navbar/component/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import logo from "../../../images/one.ai_Logo.svg"
+import menu from "../../../images/menuburger.png"
+import { NavLink } from "react-router-dom";
+const Navigation = ({open, setOpen}) => {
+    let Links =[
+      {name:"Home",link:"/"},
+      {name:"Feature",link:"/development"},
+      {name:"User Guide",link:"/"},
+      
+    ];
+    
+  return (
+    <div className='shadow-md w-full sticky top-0 left-0  z-50 nav'>
+      <div className='md:flex items-center justify-between bg-white py-[20px] pb-[17px] md:px-10 px-7 nav'>
+      <div className='font-light text-2xl cursor-pointer flex items-center text-gray-800 2xl:m-[16px]'>
+        <div className='flex text-3xl text-indigo-600 mr-1 pt-2'> 
+            <img src={logo} className='w-[32px] h-[32px]' alt='noimg'/>
+            <h1 className="DarkerGrostesque font-[600px] text-[#E4E8EB] px-[10px] mt-[-5px]">1.ai</h1>
+        </div>
+        
+      </div>
+      
+      <div onClick={()=> {setOpen(!open); } } className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+         <img src={menu} className='w-[20px] h-[20px] mt-[7px]' alt='noimg'  name={open ? 'close':'menu'} />
+          {/* <ion-icon name={open ? 'close':'menu'} ></ion-icon> */}
+      </div>
+      
+      <ul className={`float-right md:flex md:float-end md:pb-0 pb-12 absolute md:static nav md:z-auto z-[-1] left-0 w-full md:w-auto  pl-9 md:gap-[30px] xl:gap-[50px] 2xl:gap-[100px] ${open ? 'top-20 ':'top-[-490px]'}`}>
+        {
+          Links.map((link)=>(
+            <li key={link.name} className='text-white  md:ml-8 text-xl md:my-0 my-7'>
+                <NavLink to={link.link} className='cool-link  text-white text-2xl lg:text-xl font-medium leading-6 hover:text-gray-400  md:text-[15px] 2xl:text-[25px]'>{link.name} </NavLink>
+              {/* <a href={link.link} className='font font-light  text-white text-2xl lg:text-xl font-[500px] leading-6 text-gray-900 text-gray-800 hover:text-gray-400  md:text-[15px] 2xl:text-[25px]'>{link.name}</a> */}
+            </li>
+          ))
+        }
+      
+       </ul>
+      </div>
+    </div>  
+    
+  )
+}
+export default Navigation
+
