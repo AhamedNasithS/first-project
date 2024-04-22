a09ab72aea0005efcca7586c1f51c518e0aa207a
nasith <nasith@pepul.com>
2024-04-22 10:30:58 +0530

    remove landing1 page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from './pages/landing';
-import Landing1 from './pages/landing1';
-import ContactUs from './pages/contact';
+// import Landing1 from './pages/landing1';
+// import ContactUs from './pages/contact';
 import Refund from './pages/refund';
 import Privacy from './pages/privacy';
 import Terms from './pages/terms';
 import Home from './pages/home 2';
-import Navbar from './pages/navbar';
+// import Navbar from './pages/navbar';
 import Main from './pages/main';
 import Main1 from './pages/main1';
 
@@ -20,7 +20,7 @@ export default function App() {
         <Route path='/home' element={<Home/>}/>
         <Route path='/main' element={<Main/>}/>
         <Route path='/main1' element={<Main1/>}/>
-        <Route path='/development' element={<Landing1/>}/>
+        {/* <Route path='/development' element={<Landing1/>}/> */}
         {/* <Route path='/ContactUs' element={<ContactUs/>}/> */}
         <Route path='/refund' element={<Refund/>}/>
         <Route path='/privacy' element={<Privacy/>}/>
