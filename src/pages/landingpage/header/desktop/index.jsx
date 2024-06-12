5bb1eaab7ec47c93c98d49d7e622fd964c6c3ddc
nasith <nasith@pepul.com>
2024-06-12 11:51:35 +0530

    header content removes

--- src/pages/landingpage/header/desktop/index.jsx
+++ src/pages/landingpage/header/desktop/index.jsx
@@ -61,8 +61,8 @@ export default function Desktop({ setIsForm }) {
                 {/* <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer'>Product</h3> */}
                 {/* <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={() => { handleFeatureClick() }}>Features</h3> */}
                 {/* <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={() => { handlePricingClick() }}>Pricing</h3> */}
-                <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={(e) => {handleBlog(e)}}>Blog</h3>
-                <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={() => { setIsForm(true) }}>Enterprise</h3>
+                {/* <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={(e) => {handleBlog(e)}}>Blog</h3>
+                <h3 className='text-[#E2E8FFBF] font-normal text-[14px] cursor-pointer' onClick={() => { setIsForm(true) }}>Enterprise</h3> */}
             </div>
             <div className='flex gap-[14px]'>
                 <button className='py-[8px] px-[12px] w-[128px] h-[38px] lg:w-auto lg:h-auto text-[#1B1600] text-[14px] font-semibold rounded-[8px] bg-gradient-to-bl from-[#FDD01F] via-[#F8C240] to-[#F8861B]' onClick={(e) =>{handleClick(e)}}>Sign up for free</button>
